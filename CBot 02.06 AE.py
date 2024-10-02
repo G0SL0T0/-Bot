fea2f95b79939904de546d29d1a44b44ecb9ff00
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2024-10-02 20:50:50 +0300

    Update CBot 02.06 AE.py

--- CBot 02.06 AE.py
+++ CBot 02.06 AE.py
@@ -2,90 +2,62 @@ import json
 import discord
 from discord.ext import commands
 
-class BankAccount:
-    def __init__(self, owner, balance=0):
-        self.owner = owner
-        self.balance = balance
+@bot.command(name='help')
+async def help(ctx):
+    guide = """
+**Команды для работы с Жрунами**
 
-    def deposit(self, amount):
-        self.balance += amount
-        return self.balance
+**Выдать Жруны**
+`!выдать <пользователь> <количество>` - начислить Жруны пользователю
 
-    def withdraw(self, amount):
-        if amount > self.balance:
-            raise ValueError("Insufficient funds")
-        self.balance -= amount
-        return self.balance
+**Проверить баланс**
+`!баланс <пользователь>` - проверить баланс пользователя
 
-    def get_balance(self):
-        return self.balance
+**Положить Жруны**
+`!положить <пользователь> <количество>` - положить Жруны на счет пользователя
 
-class Bank:
-    def __init__(self, filename='bank.json'):
-        self.filename = filename
-        self.accounts = {}
-        self.load_accounts()
+**Снять Жруны**
+`!снять <пользователь> <количество>` - снять Жруны с счета пользователя
 
-    def load_accounts(self):
-        if os.path.exists(self.filename):
-            with open(self.filename, 'r') as f:
-                data = json.load(f)
-                for owner, account_data in data.items():
-                    account = BankAccount(owner, account_data['balance'])
-                    self.accounts[owner] = account
+**Примеры**
+`!выдать @user 10` - начислить 10 Жрунов пользователю @user
+`!баланс @user` - проверить баланс пользователя @user
+`!положить @user 5` - положить 5 Жрунов на счет пользователя @user
+`!снять @user 3` - снять 3 Жруны с счета пользователя @user
+"""
+    await ctx.send(guide)
 
-    def save_accounts(self):
-        data = {}
-        for owner, account in self.accounts.items():
-            data[owner] = {'balance': account.get_balance()}
-        with open(self.filename, 'w') as f:
-            json.dump(data, f)
-
-    def create_account(self, owner):
-        if owner not in self.accounts:
-            self.accounts[owner] = BankAccount(owner)
-            self.save_accounts()
-        return self.accounts[owner]
-
-    def get_account(self, owner):
-        return self.accounts.get(owner)
-
-    def delete_account(self, owner):
-        if owner in self.accounts:
-            del self.accounts[owner]
-            self.save_accounts()
-
-
-@bot.command(name='create-account')
-async def create_account(ctx, owner: str):
-    account = bank.create_account(owner)
-    await ctx.send(f'Account created for {owner} with balance {account.get_balance()}')
-
-@bot.command(name='deposit')
-async def deposit(ctx, owner: str, amount: int):
-    account = bank.get_account(owner)
-    if account:
-        account.deposit(amount)
-        await ctx.send(f'Deposited {amount} into {owner}\'s account. New balance: {account.get_balance()}')
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Неправильный формат команды! Используйте `!help` для справки.")
     else:
-        await ctx.send(f'Account for {owner} does not exist')
+        await ctx.send("Ошибка при выполнении команды! Используйте `!help` для справки.")
 
-@bot.command(name='withdraw')
-async def withdraw(ctx, owner: str, amount: int):
-    account = bank.get_account(owner)
-    if account:
-        try:
-            account.withdraw(amount)
-            await ctx.send(f'Withdrew {amount} from {owner}\'s account. New balance: {account.get_balance()}')
-        except ValueError as e:
-            await ctx.send(str(e))
-    else:
-        await ctx.send(f'Account for {owner} does not exist')
+# Your commands here...
 
-@bot.command(name='balance')
-async def balance(ctx, owner: str):
-    account = bank.get_account(owner)
-    if account:
-        await ctx.send(f'Balance for {owner}: {account.get_balance()}')
-    else:
-        await ctx.send(f'Account for {owner} does not exist')
+@bot.command(name='выдать')
+async def give_jrun(ctx, user: discord.Member, amount: int = 1):
+    jrun.give_jrun(user.id, amount)
+    await ctx.send(f'Начислено {amount} Жрунов пользователю {user.mention}!')
+
+@bot.command(name='баланс')
+async def balance(ctx, user: discord.Member):
+    bank = Bank('JavaS/Jrun_balance.json')
+    balance = bank.get_balance(user.id)
+    await ctx.send(f'Баланс пользователя {user.mention}: {balance} Жрунов')
+
+@bot.command(name='положить')
+async def deposit(ctx, user: discord.Member, amount: int):
+    account = BankAccount('JavaS/Jrun_balance.json', user.id)
+    account.deposit(amount)
+    await ctx.send(f'Начислено {amount} Жрунов на счет пользователя {user.mention}!')
+
+@bot.command(name='снять')
+async def withdraw(ctx, user: discord.Member, amount: int):
+    account = BankAccount('JavaS/Jrun_balance.json', user.id)
+    try:
+        account.withdraw(amount)
+        await ctx.send(f'Снято {amount} Жрунов с счета пользователя {user.mention}!')
+    except ValueError:
+        await ctx.send('Недостаточно средств на счете!')
